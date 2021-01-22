93ca7e828008caa490841ebedb40299b7aec0fa8
Jascha Ephraim <jascha@jaschaephraim.com>
2021-01-22 01:37:18 -0800

    Initial commit

---
+++ Source/EndlessRimworld.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace EndlessRimworld
+{
+    [HarmonyPatch(typeof(GameEnder), "CheckOrUpdateGameOver"), StaticConstructorOnStartup]
+    public static class Patch_GameEnder
+    {
+        static Patch_GameEnder()
+        {
+            Harmony harmony = new Harmony("com.github.jaschaephraim.endless-rimworld");
+            harmony.PatchAll();
+        }
+
+        static void Postfix()
+        {
+            GameEnder gameEnder = Find.GameEnder;
+            if (gameEnder.gameEnding)
+            {
+                gameEnder.gameEnding = false;
+
+                IncidentDef wandererJoin = IncidentDefOf.WandererJoin;
+                Map anyPlayerHomeMap = Find.AnyPlayerHomeMap;
+                IncidentParms parms = StorytellerUtility.DefaultParmsNow(wandererJoin.category, anyPlayerHomeMap);
+                Find.Storyteller.incidentQueue.Add(wandererJoin, 0, parms);
+            }
+        }
+    }
+}
